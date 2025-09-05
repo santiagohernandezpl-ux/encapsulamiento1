19213760a7ca36467ea9d57827feefe591640fd1
santiagohernandezpl-ux <santiago_hernandezpl@fet.edu.co>
2025-09-05 18:08:54 -0500

    Update CuentaBancaria.java

--- CuentaBancaria.java
+++ CuentaBancaria.java
@@ -1,1 +1,25 @@
+public class CuentaBancaria {
+   
+    private double saldo;
 
+
+    public void depositar(double cantidad) {
+        if (cantidad > 0) {
+            saldo += cantidad;
+        }
+    }
+
+
+    public boolean retirar(double cantidad) {
+        if (cantidad > 0 && cantidad <= saldo) {
+            saldo -= cantidad;
+            return true;
+        }
+        return false;
+    }
+
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
